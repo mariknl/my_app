5bb04423101ffaa990b3072af86790262584d388
marik <167160812+mariknl@users.noreply.github.com>
2024-06-15 05:46:53 +0000

    selesai kerangka file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-data = {full_name: 'Budi Gunawan', email: 'budigunawang@gmail.com',password: '1234abc'}
-user = User.create(data)
