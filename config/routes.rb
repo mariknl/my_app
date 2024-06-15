7359ab902ffb4b3ade1d1bbddde18cbc36cafbcc
marik <167160812+mariknl@users.noreply.github.com>
2024-06-15 07:49:32 +0000

    selesai halaman baca

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-
-  
   get 'home/index'
+  get '/read/:article_id', to: 'home#read', as: "read"
   resources :articles
   resources :users
+
   root to: 'home#index'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
