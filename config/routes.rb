6e2a06b2b684c9b2e3c3e80ba89a6019ae58197f
marik <167160812+mariknl@users.noreply.github.com>
2024-06-16 20:07:23 +0000

    selesai membuat kerangka file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get '/read/:article_id', to: 'home#read', as: "read"
   resources :articles
   resources :users
+  get 'read/:article_id', to: 'home#read', as: "read"
 
-  root to: 'home#index'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
